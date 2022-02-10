0590624a9642a948043f6fab76fa4a65e290d6bd
ETCWafa <lise@etcinfo.fr>
2022-02-10 17:08:27 +0100

    add loading page

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,12 +2,14 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { SignInComponent } from "./auth/sign-in/sign-in.component";
 import { SignUpComponent } from "./auth/sign-up/sign-up.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 import { AuthGuardService } from "./services/auth-guard.service";
 
 const appRoutes: Routes = [
     {path: 'todos', canActivate: [AuthGuardService], loadChildren: () => import('./todo-list/todo-list.module').then(m =>m.TodoListModule)},
     {path: 'auth/signup', component: SignUpComponent},
     {path: 'auth/signin', component: SignInComponent},
+    {path: '', component: LandingPageComponent},
 ]
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { HeaderComponent } from './header/header.component';
     SignInComponent,
     SignUpComponent,
     HeaderComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
